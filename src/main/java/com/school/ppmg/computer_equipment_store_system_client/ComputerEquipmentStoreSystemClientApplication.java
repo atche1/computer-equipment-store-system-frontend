cfa4690d4a9c2aaf606d460d324aa04180ea5f4d
atche1 <atche120207@gmail.com>
2026-01-17 20:31:13 +0200

    Initial commit - frontend

---
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_client/ComputerEquipmentStoreSystemClientApplication.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.computer_equipment_store_system_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerEquipmentStoreSystemClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerEquipmentStoreSystemClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/school/ppmg/computer_equipment_store_system_client/ComputerEquipmentStoreSystemClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.ppmg.computer_equipment_store_system_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerEquipmentStoreSystemClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
