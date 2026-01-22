ab65b6886d12e6ede0d0a1daa441c4618cbf6427
atche1 <atche120207@gmail.com>
2026-01-22 10:47:22 +0200

    add connection

--- src/main/java/com/school/ppmg/computer_equipment_store_system_client/ComputerEquipmentStoreSystemClientApplication.java
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_client/ComputerEquipmentStoreSystemClientApplication.java
@@ -2,8 +2,10 @@ package com.school.ppmg.computer_equipment_store_system_client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.school.ppmg.computer_equipment_store_system_client.client")
 public class ComputerEquipmentStoreSystemClientApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_client/config/AppConfig.java
@@ -0,0 +1,51 @@
+package com.school.ppmg.computer_equipment_store_system_client.config;
+
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.codec.ErrorDecoder;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+    @Bean
+    public RequestInterceptor feignRequestInterceptor() {
+        return requestTemplate -> {
+            ServletRequestAttributes attrs =
+                    (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+
+            if (attrs == null) {
+                return;
+            }
+
+            HttpServletRequest request = attrs.getRequest();
+
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null) {
+                requestTemplate.header("Authorization", authHeader);
+            }
+            String cookie = request.getHeader("Cookie");
+            if (cookie != null) {
+                requestTemplate.header("Cookie", cookie);
+            }
+        };
+    }
+    @Bean
+    public ErrorDecoder feignErrorDecoder() {
+        return (methodKey, response) -> switch (response.status()) {
+            case 400 -> new RuntimeException("Bad request");
+            case 401 -> new RuntimeException("Unauthorized");
+            case 403 -> new RuntimeException("Forbidden");
+            case 404 -> new RuntimeException("Not found");
+            case 500 -> new RuntimeException("Backend error");
+            default -> new RuntimeException("Feign error: " + response.status());
+        };
+    }
+}
