32d91e062c249f04c886342ae051c5bfc9348369
atche1 <atche120207@gmail.com>
2026-01-27 20:36:25 +0200

    add connection

---
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_client/enums/AttributeDataType.java
@@ -0,0 +1,5 @@
+package com.school.ppmg.computer_equipment_store_system_client.enums;
+
+public enum AttributeDataType {
+    TEXT, NUMBER, BOOLEAN
+}
---
+++ src/main/java/com/school/ppmg/computer_equipment_store_system_client/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.school.ppmg.computer_equipment_store_system_client.enums;
+
+public enum OrderStatus {
+    NEW, PROCESSING, SHIPPED, DELIVERED
+}
